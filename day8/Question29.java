35e22bdca427b9aa8ad696ed582d3ab8382a9b9f
Naman <25114CN288@glbitm.ac.in>
2026-06-11 21:14:40 +0530

    Day-8 Uploaded

---
+++ day8/Question29.java
@@ -0,0 +1,28 @@
+package day8;
+
+import java.util.Scanner;
+
+public class Question29 {
+    public static void main(String[] args) {
+
+        int rows;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        rows = sc.nextInt();
+
+        if (rows <= 0) {
+            System.out.println("Invalid input.");
+            sc.close();
+            return;
+        }
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
---
+++ day8/Question31.java
@@ -0,0 +1,30 @@
+package day8;
+
+import java.util.Scanner;
+
+public class Question31 {
+    public static void main(String[] args) {
+
+        int rows;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        rows = sc.nextInt();
+
+        if (rows <= 0) {
+            System.out.println("Invalid input.");
+            sc.close();
+            return;
+        }
+
+        for (int i = 1; i <= rows; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++ ;
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
